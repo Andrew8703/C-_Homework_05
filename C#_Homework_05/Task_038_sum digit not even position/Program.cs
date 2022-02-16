2b43a568bfcd0fb696d6b4e702e1b22ecabe772b
Andrew <and_0@bk.ru>
2022-02-16 17:04:57 +0400

    Сделал Задачу 38: Найти сумму чисел одномерного массива, стоящих на нечётной позиции

---
+++ C#_Homework_05/Task_038_sum digit not even position/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 38: Найти сумму чисел одномерного массива, стоящих на нечётной позиции.
+int[] array = new int[10];
+int sum = 0;
+
+int[] FillArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+        mass[i] = new Random().Next(0, 10);
+    return mass;
+}
+void PrintArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Console.ForegroundColor=ConsoleColor.Red;  
+            Console.Write($" {mass[i]} ");
+            Console.ResetColor();
+        }
+        else
+        {
+        Console.Write($" {mass[i]} ");
+        }
+    }    
+}
+
+int[] SumNotEvenPositions(int[] mass)
+{
+    for (int i = 1; i < mass.Length; i = i + 2)
+        sum = sum + mass[i];
+        Console.Write("\nСумма элементов стоящих на нечетных позициях массива: ");
+        Console.ForegroundColor=ConsoleColor.Red; 
+        Console.Write(sum);
+        Console.ResetColor();
+    return mass;
+}
+FillArray(array);
+Console.Write("Массив: [");
+PrintArray(array);
+Console.Write("]");
+SumNotEvenPositions(array);
+
+
+
+
+
