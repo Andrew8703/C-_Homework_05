485fc87018cd570435fac2ed420509e44a37bb0e
Andrew <and_0@bk.ru>
2022-02-16 14:34:58 +0400

    Сделал Задачу 34: написать программу для замены элементов массива на противоположные

---
+++ C#_Homework_05/Task_034_opposite array/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 34: Написать программу для замены элементов массива на противоположные.
+int[] array = new int[10];
+
+int[] FillArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+        mass[i] = new Random().Next(-100, 100);
+    return mass;
+}
+void PrintArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+        Console.Write($" {mass[i]} ");
+}
+int[] FilloppositeArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+        mass[i] = mass[i]*(-1);
+    return mass;
+}
+FillArray(array);
+Console.Write("Оригинальный массив:                  [");
+PrintArray(array);
+Console.Write("]");
+FilloppositeArray(array);
+Console.WriteLine();
+Console.Write("Массив с противоположными элементами: [");
+PrintArray(array);
+Console.Write("]");
+
+
